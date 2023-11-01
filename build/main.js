45c7132dcfda205f08c38b0d7147d6e0fee2aa19
Trixx34 <trixxdev034@gmail.com>
2023-11-01 09:58:20 +0100

    chore: release v1.2.3
    - Typo in main which caused application crashes

--- build/main.js
+++ build/main.js
@@ -89,7 +89,7 @@ class soliscloud extends utils.Adapter {
           write: false
         },
         native: {}
-      },);
+      });
       await this.setObjectNotExistsAsync(
         `${this.config.plantId}.generated_today`,
         {
